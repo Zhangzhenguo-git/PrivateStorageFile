a87453c1c9e21e75534a7aa847ac269c8684e56c
kyzgxiaoguo <826189539@qq.com>
2020-04-17 15:03:45 +0800

    适配androidQ存储域

--- app/src/main/java/com/zhangzhenguo/privatestoragefile/MainActivity.java
+++ app/src/main/java/com/zhangzhenguo/privatestoragefile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zhangzhenguo.privatestoragefile;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
@@ -16,38 +17,108 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.tbruyelle.rxpermissions2.Permission;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
 import java.io.File;
 import java.io.IOException;
+
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btCreateFiles;
     private Button btCreateFile;
+    private Button btCreateFileD;
     private TextView tvShowPath;
     private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context=MainActivity.this;
         btCreateFiles=findViewById(R.id.btCreateFiles);
         btCreateFile=findViewById(R.id.btCreateFile);
+        btCreateFileD=findViewById(R.id.btCreateFileD);
         tvShowPath=findViewById(R.id.tvShowPath);
+        openPermissions();
+        context=MainActivity.this;
+    }
+
+    private void muCLick(){
         btCreateFiles.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                File files=new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath()+"/hgmm/Upload/");
-                if (files.exists()){
+                File files=new File(Environment.getExternalStorageDirectory().getPath()+"/test/Upload");
+                if (!files.exists()){
                     files.mkdir();
                     Toast.makeText(context, "创建文件夹成功"+files.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(context, "已有此文件夹"+files.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                }
+                File file=new File(Environment.getExternalStorageDirectory().getPath()+"/test/Upload/111.zip");
+                if (!file.exists()){
+                    try {
+                        file.createNewFile();
+                        Toast.makeText(context, "创建文件成功"+file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                    Toast.makeText(context, "已有此文件或创建失败"+file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
         btCreateFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                File file=new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath()+"/hgmm/Upload/111.zip");
+                String path=getSDPath(context);
+                Log.d("执行",path);
+                File files=new File(path+"/test/Upload/");
+                if (!files.exists()){
+                    files.mkdirs();
+                    Toast.makeText(context, "创建文件夹成功"+files.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                }
+                File file=new File(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS).getPath()+"/test/Upload/111.zip");
                 if (!file.exists()){
+                    try {
+                        file.createNewFile();
+                        Toast.makeText(context, "创建文件成功"+file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                    try {
+                        file.createNewFile();
+                        Toast.makeText(context, "创建文件成功"+file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        btCreateFileD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String path=getSDPath(context);
+                Log.d("执行",path);
+                File files=new File(path+"/test/");
+                if (!files.exists()){
+                    files.mkdir();
+                    File files1=new File(path+"/test/Upload/");
+                    if (!files1.exists()){
+                        files1.mkdir();
+                        Log.d("执行","创建文件夹成功"+files1.getAbsolutePath());
+                    }
+                }
+                File file=new File(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS).getPath()+"/test/Upload/111.zip");
+                if (!file.exists()){
+                    try {
+                        file.createNewFile();
+                        Toast.makeText(context, "创建文件成功"+file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }else {
                     try {
                         file.createNewFile();
                         Toast.makeText(context, "创建文件成功"+file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
@@ -59,19 +130,32 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    public static String getSDPath(Context context) {
+        File sdDir = null;
+        boolean sdCardExist = Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED);// 判断sd卡是否存在
+        if (sdCardExist) {
+            if (Build.VERSION.SDK_INT>=29){
+                //Android10之后
+                sdDir = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+            }else {
+                sdDir = Environment.getExternalStorageDirectory();// 获取SD卡根目录
+            }
+        } else {
+            sdDir = Environment.getRootDirectory();// 获取跟目录
+        }
+        return sdDir.toString();
+    }
     /**
      * 打开权限
      */
     private void openPermissions(){
-        final RxPermissions rxPermissions = new RxPermissions(LoadingActivity.this); // where this is an Activity or Fragment instance
+        final RxPermissions rxPermissions = new RxPermissions(MainActivity.this); // where this is an Activity or Fragment instance
         rxPermissions.requestEachCombined(
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,//写入
                 Manifest.permission.READ_EXTERNAL_STORAGE,//读取
-                Manifest.permission.CAMERA,//相机
-                Manifest.permission.ACCESS_COARSE_LOCATION,//获取当前系统位置信息
-                Manifest.permission.RECORD_AUDIO,//录音权限
-                Manifest.permission.READ_PHONE_STATE,//获取设备信息
-                Manifest.permission.SEND_SMS//收发短信权限
+                Manifest.permission.READ_PHONE_STATE//获取设备信息
 //                Manifest.permission.REQUEST_INSTALL_PACKAGES//允许未知来源安装
 
 
@@ -89,21 +173,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void accept(Permission permission) throws Exception {
                 if (permission.granted){
-                    //        android8.0未知来源安装适配
-                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                        boolean haveInstallPermission = getPackageManager().canRequestPackageInstalls();
-                        if(!haveInstallPermission){
-                            Log.d("执行","转到设置");
-                            //权限没有打开则提示用户去手动打开
-                            Uri packageURI = Uri.parse("package:"+LoadingActivity.this.getPackageName());
-                            Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES,packageURI);
-                            startActivityForResult(intent, INSTALL_PERMISS_CODE);
-                        }else {
-                            Log.d("执行","通过了所有权限");
-                        }
-                    }else {
                         Log.d("执行","通过了所有权限");
-                    }
+                        muCLick();
                 }else if (permission.shouldShowRequestPermissionRationale){
                     Log.d("执行","至少有一个权限被拒绝了");
                     openPermissions();
